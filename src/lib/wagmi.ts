22b12318bbb87e1f84382b4bc1dec3bd264e3a87
jennifertrin <jennifer.tran.2017@trincoll.edu>
2025-05-22 22:03:27 -0700

    chore: update farcaster specs and comment out nft related feature

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,13 +1,14 @@
 import { http, createConfig } from 'wagmi'
 import { base } from 'wagmi/chains'
-import { farcasterFrame as miniAppConnector } from '@farcaster/frame-wagmi-connector'
+// import { farcasterFrame as miniAppConnector } from '@farcaster/frame-wagmi-connector'
  
 export const config = createConfig({
   chains: [base],
   transports: {
     [base.id]: http(),
-  },
-  connectors: [
-    miniAppConnector()
-  ]
+  }
+  // ,
+  // connectors: [
+  //   miniAppConnector()
+  // ]
 })
